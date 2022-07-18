4b9bfa501c962ed693139cb8869271af0d376a75
almouro <contact@almouro.com>
2022-07-18 20:17:20 +0200

    4. high algorithmic complexity

--- src/feed/filterTweets.ts
+++ src/feed/filterTweets.ts
@@ -1,16 +1,6 @@
-import {format, parse} from 'date-fns';
+import {isWeekend} from 'date-fns';
 import {Tweet} from '../Tweet';
 
-const getTweetWeekDay = (tweet: Tweet) =>
-  format(
-    parse(tweet.createdAt, "yyyy-MM-dd'T'HH:mm:ss.SSSX", new Date()),
-    'EEEE',
-  );
-
 export const filterTweets = (tweets: Tweet[]) => {
-  return tweets.filter(tweet => {
-    return !!['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].find(
-      weekDay => weekDay === getTweetWeekDay(tweet),
-    );
-  });
+  return tweets.filter(tweet => !isWeekend(new Date(tweet.createdAt)));
 };
