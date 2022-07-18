f7f0612f8216f7e8341f0e5657e07b32caa41356
almouro <contact@almouro.com>
2022-07-18 20:22:38 +0200

    6 - UI thread

--- src/ListContainer.tsx
+++ src/ListContainer.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
 import SkeletonContent from 'react-native-skeleton-placeholder';
 
-export const ListContainer = ({children, style}: any) => {
+export const SkeletonContainer = () => {
   return (
-    <View style={style}>
-      <View style={StyleSheet.absoluteFill}>
-        <SkeletonContent>
-          {Array(3)
-            .fill(null)
-            .map((_, index) => (
-              <SkeletonContent.Item
-                flexDirection="row"
-                alignItems="center"
-                margin={10}
-                key={index}>
-                <SkeletonContent.Item
-                  width={60}
-                  height={60}
-                  borderRadius={50}
-                  marginRight={20}
-                />
-                <SkeletonContent.Item
-                  width={250}
-                  height={200}
-                  borderRadius={4}
-                />
-              </SkeletonContent.Item>
-            ))}
-        </SkeletonContent>
-      </View>
-      {children}
-    </View>
+    <SkeletonContent>
+      {Array(3)
+        .fill(null)
+        .map((_, index) => (
+          <SkeletonContent.Item
+            flexDirection="row"
+            alignItems="center"
+            margin={10}
+            key={index}>
+            <SkeletonContent.Item
+              width={60}
+              height={60}
+              borderRadius={50}
+              marginRight={20}
+            />
+            <SkeletonContent.Item width={250} height={200} borderRadius={4} />
+          </SkeletonContent.Item>
+        ))}
+    </SkeletonContent>
   );
 };
