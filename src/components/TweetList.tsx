9ec35b2fd9bfead3db85c9539878d1fdd79a2515
almouro <contact@almouro.com>
2022-07-18 20:19:41 +0200

    5 - FlashList

--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -1,27 +1,24 @@
-import React, {memo} from 'react';
-import {FlatList, View} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 import {Feed, Tweet} from '../Tweet';
 import {TweetItem} from './TweetItem';
+import {FlashList} from '@shopify/flash-list';
 
-const ItemSeparatorComponent = memo(() => (
+const ItemSeparatorComponent = () => (
   <View style={{height: 1, backgroundColor: '#eee', marginVertical: 10}} />
-));
-
-const MemoizedTweetItem = memo(TweetItem);
-
-const renderItem = ({item}: {item: Tweet}) => (
-  <MemoizedTweetItem tweet={item} />
 );
+
+const renderItem = ({item}: {item: Tweet}) => <TweetItem tweet={item} />;
 const keyExtractor = ({id}: Tweet) => id;
 
 export const TweetList = ({feed}: {feed: Feed}) => {
   return (
-    <FlatList
+    <FlashList
+      estimatedItemSize={360}
       contentContainerStyle={{
         paddingVertical: 10,
         backgroundColor: 'white',
       }}
-      style={{backgroundColor: 'white', flex: 1}}
       ItemSeparatorComponent={ItemSeparatorComponent}
       keyExtractor={keyExtractor}
       renderItem={renderItem}
